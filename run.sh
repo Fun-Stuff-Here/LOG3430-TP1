3504d4d5cb92898a1233a36777db20cc3aae9e41
Nicolas Depelteau <71102008+Depdx@users.noreply.github.com>
2022-01-31 08:45:13 -0500

    ND: update run script

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-python3 -m unittest test_crud.py
-coverage run -m unittest test_crud.py
+python3 -m unittest test_crud.py test_email_analyzer.py test_vocabulary_creator.py
+coverage run -m unittest test_crud.py test_email_analyzer.py test_vocabulary_creator.py
 coverage report
+coverage html
 cmd.exe
